977694849bb5b04d0c1c1746a2d2807a52b286f7
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-10-29 11:52:12 -0300

    First commit

---
+++ edux-react/src/pages/home/index.jsx
@@ -0,0 +1,427 @@
+<head>
+<title>Schooling a Education School Category Flat Bootstrap Responsive Web Template| Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1"/>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Schooling Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0)}, false);
+function hideURLbar(){ window.scrollTo(0,1)} </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link rel="stylesheet" href="css/jquery-ui.css" type='text/css' />
+<link rel="stylesheet" href="css/smoothbox.css" type='text/css' />
+
+<script src="js/jquery.min.js"></script>
+
+<link href='//fonts.googleapis.com/css?family=Gloria+Hallelujah' rel='stylesheet' type='text/css'/>
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'/>
+
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+
+<head/>
+<body>
+	<script src="js/jquery.vide.min.js"></script>
+	<div data-vide-bg="video/srix">
+
+		<div classname="center-container">
+
+			<div class="navigation">
+					<div class="container">
+						<div class="logo">
+							<img src="../home/images/logo_2.png" alt=""/>
+						<div/>
+
+						<div class="navigation-right">
+							<span class="menu"><img src="images/menu.png" alt=" " /></span>
+							<nav class="link-effect-3" id="link-effect-3">
+								<ul class="nav1 nav nav-wil">
+									<li class="active"><a data-hover="Home" href="index.html">Home</a></li>
+									<li><a data-hover="About" href="#about" class="scroll">About</a></li>
+									<li><a data-hover="Team" href="#team" class="scroll">Team</a></li>
+									<li><a data-hover="Gallery" href="#gallery" class="scroll">Gallery</a></li>
+									<li><a data-hover="Contact" href="#contact" class="scroll">Contact</a></li>
+								<ul/>
+							<nav/>
+
+								<script>
+								   $( "span.menu" ).click(function() {
+									 $( "ul.nav1" ).slideToggle( 300, function() {
+									 // Animation complete.
+									  })
+									 });
+								</script>
+					<div/>
+					<div class="clearfix"></div>
+				<div/>
+			<div/>
+			<div class="w3ls_banner_info">
+				<div class="w3l_banner_logo">
+					<img src="../home/images/logo_2.png" alt="">
+				<div/>
+
+
+				<div class="container">
+					<h3>A melhor oportunidade para evoluir com a nossa nova plataforma</h3>
+					<p>veja as dicas de seu professor, sua sala, suas tarefas feitas, e sua nota!!!</p>
+					<div class="more">
+						<a href="#" class="hvr-underline-from-center" data-toggle="modal" data-target="#myModal">Sobre</a>
+					</div>
+					<div class="modal video-modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModal">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content">
+								<div class="modal-header">
+									Projeto EDUX
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>						
+								</div>
+								<section>
+									<div class="modal-body">
+										<img src="images/17.jpg" alt=" " class="img-responsive" />
+										<p>Ut enim ad minima veniam, quis nostrum 
+											exercitationem ullam corporis suscipit laboriosam, 
+											nisi ut aliquid ex ea commodi consequatur? Quis autem 
+											vel eum iure reprehenderit qui in ea voluptate velit 
+											esse quam nihil molestiae consequatur, vel illum qui 
+											dolorem eum fugiat quo voluptas nulla pariatur.
+											<i>" Quis autem vel eum iure reprehenderit qui in ea voluptate velit 
+												esse quam nihil molestiae consequatur.</i></p>
+									</div>
+								</section>
+							</div>
+						</div>
+					</div>
+				</div>
+			<div/>
+		<div/>
+	<div/>
+
+	<div class="about">
+		<div class="container">
+			<div class="agileits_about_grids">
+				<div class="col-md-6 agileits_about_grid_left">
+					<h3>What We Offer</h3>
+					<p>Duis aute irure dolor in reprehenderit 
+						in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+						Excepteur sint occaecat cupidatat non proident, sunt in culpa qui 
+						officia deserunt mollit anim id est laborum.</p>
+				</div>
+				<div class="col-md-6 agileits_about_grid_right">
+					<img src="images/16.jpg" alt=" " class="img-responsive" />
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="banner-bottom">
+		<div class="container">
+			<div class="col-md-4 wthree_banner_bottom_left">
+				<img src="images/1.jpg" alt=" " class="img-responsive" />
+			</div>
+			<div class="col-md-8 wthree_banner_bottom_right">
+				<h3>the whole purpose of education is to turn mirrors into windows</h3>
+				<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
+					dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				<ol>
+					<li>sunt in culpa qui officia deserunt</li>
+					<li>Excepteur sint occaecat cupidatat</li>
+					<li>irure dolor in reprehenderit</li>
+					<li>proident, sunt in culpa qui officia</li>
+					<li>deserunt mollit anim id est laborum</li>
+					<li>Duis aute irure dolor in reprehen</li>
+				</ol>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+
+	<div id="about" class="banner-bottom-image-text">
+		<div class="col-md-8 banner-bottom-image-text-left">
+			<h3>Who We Are</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
+				cillum dolore eu fugiat nulla pariatur.</p>
+			<h4>education is for improving the lives of others and for leaving your
+				community and world better than you found it.</h4>
+		</div>
+		<div class="clearfix"> </div>
+	</div>
+
+	<div class="team" id="team">
+		<div class="container">
+			<h3>Conheça nosso time</h3>
+			<p class="nostrud">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+			<div class="agile_team_grids">
+				<div class="col-md-3 agile_team_grid">
+					<div class="view view-sixth">
+						<img src="images/3.jpg" alt=" " class="img-responsive" />
+						<div class="mask">
+							<h5>quis nostrud</h5>
+							<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit.</p>
+							<div class="agileits_social_icons">
+								<a href="#" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+								<a href="#" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+								<a href="#" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+							</div>
+						</div>
+					</div>
+					<h4>John Doe</h4>
+					<p>Professor</p>
+				</div>
+				<div class="col-md-3 agile_team_grid">
+					<div class="view view-sixth">
+						<img src="images/6.jpg" alt=" " class="img-responsive" />
+						<div class="mask">
+							<h5>quis nostrud</h5>
+							<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit.</p>
+							<div class="agileits_social_icons">
+								<a href="#" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+								<a href="#" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+								<a href="#" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+							</div>
+						</div>
+					</div>
+					<h4>Laura Carl</h4>
+					<p>Professor</p>
+				</div>
+				<div class="col-md-3 agile_team_grid">
+					<div class="view view-sixth">
+						<img src="images/4.jpg" alt=" " class="img-responsive" />
+						<div class="mask">
+							<h5>quis nostrud</h5>
+							<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit.</p>
+							<div class="agileits_social_icons">
+								<a href="#" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+								<a href="#" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+								<a href="#" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+							</div>
+						</div>
+					</div>
+					<h4>James Cam</h4>
+					<p>Professor</p>
+				</div>
+				<div class="col-md-3 agile_team_grid">
+					<div class="view view-sixth">
+						<img src="images/5.jpg" alt=" " class="img-responsive" />
+						<div class="mask">
+							<h5>quis nostrud</h5>
+							<p>Quis autem vel eum iure reprehenderit qui in ea voluptate velit.</p>
+							<div class="agileits_social_icons">
+								<a href="#" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+								<a href="#" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+								<a href="#" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+							</div>
+						</div>
+					</div>
+					<h4>Luther</h4>
+					<p>Professor</p>
+				</div>
+				<div class="clearfix"> </div>
+			</div>
+		</div>
+	</div>
+
+	<div class="gallery" id="gallery">
+		<h3>Photo Gallery</h3>
+		<p class="nostrud">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris</p>
+		<div class="w3agile_gallery_grids">
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" href="images/8.jpg" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum">
+						<figure>
+							<img src="images/8.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/9.jpg">
+						<figure>
+							<img src="images/9.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/10.jpg">
+						<figure>
+							<img src="images/10.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/11.jpg">
+						<figure>
+							<img src="images/11.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/12.jpg">
+						<figure>
+							<img src="images/12.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/13.jpg">
+						<figure>
+							<img src="images/13.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/14.jpg">
+						<figure>
+							<img src="images/14.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+			<div class="col-md-3 w3agile_gallery_grid">
+				<div class="w3agile_gallery_image">
+					<a class="sb" title="quis nostrud exercitation ullamco laboris quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum" href="images/15.jpg">
+						<figure>
+							<img src="images/15.jpg" alt="" class="img-responsive" />
+							<figcaption>
+								<h4>dolore magna aliqua</h4>
+								<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+									tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</figcaption>
+						</figure>
+					</a>
+				</div>
+			</div>
+		   <div class="clearfix"> </div>
+		</div>
+		<script type="text/javascript" src="js/smoothbox.jquery2.js"></script>
+	</div>
+
+	<div class="contact" id="contact">
+		<div class="col-md-6 w3agile_contact_left">
+			<h3>Contact Us</h3>
+			<p>Excepteur sint occaecat cupidatat non proident, sunt 
+				in culpa qui officia deserunt mollit anim id est laborum reprehenderit in voluptate velit esse 
+				cillum dolore eu fugiat nulla pariatur.</p>
+			<form action="#" method="post"/>
+				<input type="text" name="Name" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required=""/>
+				<input type="email" name="Email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required=""/>
+				<textarea name="Message" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message...';}" required="">Message...</textarea>
+				<input type="submit" value="Send"/>
+			<form/>
+		</div>
+		<div class="col-md-6 w3agile_contact_right">
+			<h3><a href="index.html">Schooling</a></h3>
+			<div class="col-xs-6 w3agile_contact_right_agileinfo">
+				<h4>Address</h4>
+				<p><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span>8921 California Long Beach <i>PO Box 8921 202 East Ocean.</i></p>
+				<p><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>(+) 0983 010 823</p>
+				<p><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span><a href="mailto:info@example.com">info@example.com</a></p>
+			</div>
+			<div class="col-xs-6 w3agile_contact_right_agileinfo"/>
+				<h4>Follow Us</h4>
+				<div class="agileits_social_icons">
+					<a href="#" class="icon-button twitter"><i class="icon-twitter"></i><span></span></a>
+					<a href="#" class="icon-button facebook"><i class="icon-facebook"></i><span></span></a>
+					<a href="#" class="icon-button google-plus"><i class="icon-google-plus"></i><span></span></a>
+				</div>
+			<div/>
+			<div class="clearfix"> </div>
+			<div class="w3_copy_right">
+				<p>© 2016 Schooling. All rights reserved | Design by <a href="http://w3layouts.com">W3layouts</a></p>
+			</div>
+		</div>
+		<div class="clearfix"> <div/>
+	</div>
+
+<script src="js/bootstrap.js"></script>
+
+	<script type="text/javascript">
+		$(document).ready(function() {
+			
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				}
+		
+								
+			$().UItoTop({ easingType: 'easeOutQuart' });
+								
+			});
+	</script>
+
+</body>
